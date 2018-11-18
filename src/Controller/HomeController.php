50552db8cbeb4f87ff0c378639146b4f99a9455f
RimvydasTa <tamosiunas.rimvydas@gmail.com>
2018-11-18 13:37:22 +0200

    Refactored home controller dependecies. Added sass structure

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,15 +1,9 @@
 <?php
 namespace App\Controller;
 use App\Services\CompetitionService;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\ChoiceList\Loader\CallbackChoiceLoader;
+use Symfony\Component\Routing\Annotation\Route;
+
 class HomeController extends Controller
 {
     private $competitionService;
