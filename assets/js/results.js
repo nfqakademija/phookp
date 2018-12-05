739ac2bda1fed4de8979a26735e894bd604db67e
Mantas Muliarcikas <mntsss@gmail.com>
2018-12-05 05:42:30 +0200

    Added responsiveness to total results table (WINDOWS)

--- assets/js/results.js
+++ assets/js/results.js
@@ -14,6 +14,11 @@ modal.addFooterBtn('Uzdaryti', 'tingle-btn tingle-btn--primary', function() {
     modal.close();
 });
 
+const modalHeader = (team) => {
+    return `<div class="bg-primary text-white h5 mb-2 results-modal__header">
+            ${team} tarpiniai rezultatai</div>`;
+};
+
 const top5ResultsTemplate = (number, weigh) => {
     return `<tr>
                 <td class="u-bg-main u-text-white">TOP ${number}</td>
@@ -22,63 +27,58 @@ const top5ResultsTemplate = (number, weigh) => {
     `;
 };
 
+const buildTop5Table = (results) =>{
+    const newTable = document.createElement('table');
+    newTable.className = 'table table-bordered';
+
+    for (let i = 0; i< 5; i++){
+        let res = 0;
+        if(typeof results[i] !== 'undefined')
+            res = results[i];
+
+        newTable.innerHTML += top5ResultsTemplate(parseInt(i)+1, res);
+    }
+    return newTable.outerHTML;
+};
+
+const buildTotalHeader = () => {
+    return `<table><tr class="row mx-0 bg-primary text-white u-text-bold">
+                <th class="col-4">Svėrimo nr.</th>
+                <th class="col-4">Svoris</th>
+                <th class="col-4">Žuvų skaičius</th>
+            </tr></table>`;
+};
+
 const totalResultsTemplate = (number, weigh, count) => {
-    return `<tr>
-                <td class="u-bg-main u-text-white">${number}</td>
-                <td class="u-text-bold"> ${weigh}kg</td>
-                <td class="u-text-bold"> ${count}kg</td>
+    return `<tr class="row mx-0">
+                <td class="col-4 u-bg-main u-text-white">${number}</td>
+                <td class="col-4 u-text-bold"> ${weigh}kg</td>
+                <td class="col-4 u-text-bold"> ${count}</td>
             </tr>
     `;
 };
 
-const buildTop5Table = () =>{
-    const newTable = document.createElement('table');
-    newTable.className = 'table table-bordered';
-    return newTable;
-};
-
-const buildTotalHeader = () => {
-    const newTable = document.createElement('table');
-    const tableHeader = document.createElement('thead');
-    thead.innerHTML =
-        `<tr>
-            
-         </tr>
-        `;
-    newTable.classList.add('mdl-data-table');
-
-};
-
-const modalHeader = (team) => {
-  return `<div class="bg-primary text-white h5 mb-2 results-modal__header">
-            ${team} tarpiniai rezultatai</div>`;
+const buildTotalTable = (weighings) => {
+    const table = document.createElement('table');
+    for(let i = 0; i < weighings.length; i++){
+        table.innerHTML+=totalResultsTemplate(parseInt(i)+1, weighings[i][1], weighings[i][2]);
+    }
+    return buildTotalHeader()+table.outerHTML;
 };
 
 const openResultsModal = (e) => {
     if(window.screen.availWidth <= 768){
         const team = e.currentTarget.getAttribute('data-team-name');
-        let results = null;
+        let modalContent = null;
         if(e.currentTarget.hasAttribute('data-results-top5')){
-            results = JSON.parse(e.currentTarget.getAttribute('data-results-top5'));
-
+            modalContent = buildTop5Table(JSON.parse(e.currentTarget.getAttribute('data-results-top5')));
         }
         else if(e.currentTarget.hasAttribute('data-results-total')){
-            results = e.currentTarget.getAttribute('data-results-total');
-
+            modalContent = buildTotalTable(JSON.parse(e.currentTarget.getAttribute('data-results-total')));
         }else
             return;
 
-        const resultTable = buildTop5Table();
-
-        for (var i = 0; i< 5; i++){
-            var res = 0;
-            if(typeof results[i] !== 'undefined')
-                res = results[i];
-
-            resultTable.innerHTML += top5ResultsTemplate(parseInt(i)+1, res);
-        }
-
-        modal.setContent(modalHeader(team)+resultTable.outerHTML);
+        modal.setContent(modalHeader(team) + modalContent);
         modal.open();
     }
 };
