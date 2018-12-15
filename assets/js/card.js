6429594c66749cfe86dfeb9abc5a38de66d5251d
Mantas Muliarcikas <mntsss@gmail.com>
2018-12-15 19:01:05 +0200

    card stuff

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,10 +1,9 @@
 const coll = document.querySelectorAll(".collapsible");
-let i;
 
-for (i = 0; i < coll.length; i++) {
+for (let i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        const content = document.querySelector("#content_1");
+        const content = document.querySelector("#"+this.dataset.container);
         if (content.style.maxHeight){
             content.style.maxHeight = null;
         } else {
