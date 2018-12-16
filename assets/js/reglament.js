08e9c7cf8d40f949efffd2d283b10d7798cb8129
Mantas Muliarcikas <mntsss@gmail.com>
2018-12-16 13:42:49 +0200

    Reglament modal fix

--- assets/js/reglament.js
+++ assets/js/reglament.js
@@ -2,19 +2,29 @@ import tingle from 'tingle.js';
 
 const modalReglament = new tingle.modal({
     footer: true,
-    stickyFooter: true
+    stickyFooter: true,
+    closeMethods: [],
+    beforeClose: function () {
+        document.querySelector('nav').classList.remove('d-none');
+        return true;
+    }
 });
 
 document.addEventListener("DOMContentLoaded",function(){
 
     modalReglament.addFooterBtn('Uždaryti', 'tingle-btn tingle-btn--default tingle-btn--pull-right', function(){
-        modalStickyFooter.close();
+        modalReglament.close();
     });
     const rules = document.querySelector('#btn_rules').getAttribute('data-rules');
-    modalReglament.setContent(`<div class="text-white h5 mb-2 results-modal__header">Varžybų reglamentas</div><p class="p-3 mb-5">${rules}</p>`);
+    modalReglament.setContent(`<div class="text-white h5 mb-2 results-modal__header">Varžybų reglamentas</div><div class="table-responsive js-reglament-table">
+        <p class="p-3">${rules}</p></div>`);
 
 });
 
 document.querySelector('#btn_rules').addEventListener('click', function(){
+    const reglamentTable = document.querySelector('.js-reglament-table');
+
+    reglamentTable.style.height = screen.height / 1.5+"px";
+    document.querySelector('nav').classList.add('d-none');
     modalReglament.open();
 });
